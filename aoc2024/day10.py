0a288d1c09e9ac293d4e0b41ef037c8f527ecd48
Marcel R. <github.riga@icloud.com>
2024-12-10 10:46:28 +0100

    Day 10.

--- aoc2024/day10.py
+++ aoc2024/day10.py
@@ -12,8 +12,6 @@ from aoc2024 import Solver, Part
 
 
 def solution(data: list[str], part: Part) -> int | None:
-    imax, jmax = len(data), len(data[0])
-
     # convert to complex mapping
     heights: dict[complex, int] = {}
     for i, line in enumerate(data):
@@ -23,24 +21,21 @@ def solution(data: list[str], part: Part) -> int | None:
     # find heads
     heads = [p for p, h in heights.items() if h == 0]
 
-    # walking directions
-    directions = [1 + 0j, -1 + 0j, 1j, -1j]
-
     # walking helper returning the number of reachable tops given a starting point
-    # note: part a could be solved faster by using a decicated function that drops duplicates right away
+    # note: part A could be solved faster by using a decicated function that drops duplicates right away
     #       instead of at the end, but the penalty is not too high and the code is more readable
     def num_reachable_tops(head: complex, unique: bool) -> int:
+        directions = (1, -1, 1j, -1j)
+        # check each height
         q = deque([head])
         height = 0
-        # check each height
         while height < 9:
             height += 1
             for _ in range(len(q)):
                 h = q.popleft()
                 # check each direction
                 for d in directions:
-                    p = h + d
-                    if (0 <= p.real < imax) and (0 <= p.imag < jmax) and heights[p] == height:
+                    if heights.get(p := h + d) == height:
                         q.append(p)
         return len(set(q) if unique else q)
 
